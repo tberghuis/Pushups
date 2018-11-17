03f4ad3b99a5c1d01443b81184096d3ae1deb89c
Tom Berghuis <thomas.berghuis@gmail.com>
2018-11-17 16:18:35 +1100

    .

--- app/src/main/java/com/tomberghuis/pushups/data/CompletedPushupSet.kt
+++ app/src/main/java/com/tomberghuis/pushups/data/CompletedPushupSet.kt
@@ -1,11 +1,17 @@
 package com.tomberghuis.pushups.data
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
 
-@Entity
-data class CompletedPushupSet(@PrimaryKey(autoGenerate = true) var id: Int = 0,
-                var numPushups: Int,
+@Entity(tableName = "completed_pushup_sets")
+data class CompletedPushupSet(
+    // do i need default val = 0
+    // should i use val instead of var?
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    @ColumnInfo(name = "num_pushups") var numPushups: Int,
                               // will it blend
-                var timestampCompletedAt: Long = System.currentTimeMillis())
+    @ColumnInfo(name = "timestamp_completed_at") var timestampCompletedAt: Long = System.currentTimeMillis())
+
+
--- app/src/main/java/com/tomberghuis/pushups/data/CompletedPushupSetDao.kt
+++ app/src/main/java/com/tomberghuis/pushups/data/CompletedPushupSetDao.kt
@@ -1,8 +1,19 @@
 package com.tomberghuis.pushups.data
 
+import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
 
 @Dao
 interface CompletedPushupSetDao {
+
+
+    @Query("SELECT * FROM completed_pushup_sets order by id desc limit 1")
+    // will this blend
+    fun getLastPushupSet(): CompletedPushupSet
+
+    @Insert
+    fun insertPushupSet(pushupSet: CompletedPushupSet)
 
 }
