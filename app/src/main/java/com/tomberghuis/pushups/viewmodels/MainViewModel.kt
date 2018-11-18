406b42e16e662d0597338074387e95607797f56e
Tom Berghuis <thomas.berghuis@gmail.com>
2018-11-18 19:56:33 +1100

    .

--- app/src/main/java/com/tomberghuis/pushups/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/tomberghuis/pushups/viewmodels/MainViewModel.kt
@@ -15,41 +15,26 @@ import kotlinx.coroutines.launch
 class MainViewModel(
     private val pushupSetRepository: PushupSetRepository
 ) : ViewModel() {
-    // didn't need to use livedata for some reason, not using room
 
     // 2 way data binding number picker
-    // TODO 2 way data binding aint working
-//    var numPushupsNumberPicker: Int = 5
     var numPushupsNumberPicker = MutableLiveData<Int>()
 
     init {
+        // may not need Main dispatcher as using postValue
+        // alternative to globalscope? context as coroutinescope???
         GlobalScope.launch(Main) {
-
-
-
+            numPushupsNumberPicker.postValue(5)
+            // the following will terminate thread if database empty
+            // how, i don't know
             numPushupsNumberPicker.postValue(pushupSetRepository.getLastPushupSet().numPushups)
-////            numPushupsNumberPicker = pushupSetRepository.getLastPushupSet().numPushups
-////            Log.d("aaa",numPushupsNumberPicker.toString())
-//
-//
-////            = async(IO) {
-////                // what happens when run app for first time and db empty?
-////                // basically thread is getting killed because getLastPushupSet returns null
-////                // how does that work??? some sort of magic
-//////            pushupSetRepository.getLastPushupSet().numPushups.let {
-//////                // somehow the 2 way data binding is working even when assigning not on the UI thread
-//////                numPushupsNumberPicker = it
-//////            }
-////                pushupSetRepository.getLastPushupSet().numPushups
-////            }.await()
-//            Log.d("aaa",numPushupsNumberPicker.value.toString())
         }
-
     }
 
-    // TODO fix !!
     fun saveCompletedPushupSet(){
-        pushupSetRepository.saveCompletedPushupSet(numPushupsNumberPicker.value!!)
+        // pushupSetRepository.saveCompletedPushupSet(numPushupsNumberPicker.value!!)
+        numPushupsNumberPicker.value?.let {
+            pushupSetRepository.saveCompletedPushupSet(it)
+        }
     }
 
 }
